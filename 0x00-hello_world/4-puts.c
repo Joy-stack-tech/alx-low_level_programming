1d2a56b2134b7f6123f1220d35507eff2ae19610
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-03-16 09:50:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts instead of printf
+/**
+ * main -print a line of code using puts instead of printf
  *
- * Return: 0
+ * Return: Always 0
 */
+
 int main(void)
 {
-	puts("\"Programming is like buildinga multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
