11483263f522be035ae15001023fc68ed64e1938
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-04 19:46:40 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
+	int j = n;
 
-	while (i = 0)
+
+	for (; i < j; i++)
 	{
 		dest[i] = src[i];
-		i++;
+		n--;
 	}
 	return (dest);
 }
