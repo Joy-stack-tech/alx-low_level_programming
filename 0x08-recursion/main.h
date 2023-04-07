c95ce370e98a4a7ba1b54bbb3bf10f401767b6e8
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-07 09:26:26 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int sqrt2(int a, int b);
+int check(int a, int b);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
