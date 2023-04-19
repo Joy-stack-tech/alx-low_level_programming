f47d3fdb5769fcfd0cf6f95147a47d1b79f82d56
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-19 07:52:47 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @i: the inputted name
+ * @f: the function pointer
+ *
+ * Return: no value
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	char *i = name;
+
+	if (i && f)
+		f(i);
+}
