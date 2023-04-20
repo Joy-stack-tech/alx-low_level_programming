08cdee5050ce1aa2d09354bc935c78659eb107f7
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-20 06:46:04 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,8 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (s[0] == ops->op[i])
 			break;
+		i++;
 	}
-	i++;
-	{
-		return (ops[i / 2].f);
-	}
+
+	return (ops[i / 2].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,10 +19,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	oprt = get_op_func(argv[2]);
 
-	if (!oprt)
+	if (oprt == NULL)
 	{
 		printf("Error\n");
 		exit(99);
