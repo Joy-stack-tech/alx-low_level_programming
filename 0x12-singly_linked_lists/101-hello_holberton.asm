bb511eb40f719d9d89aa8c8b9aa838b20b683104
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-28 08:30:34 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,13 +3,12 @@ section .data ; data section for data input/initialization
 format db "Hello, Holberton", 0 ; format string that will be passed to func printf
 fmt db '%s\n', 0
 section .text ; input code here
-
 global main ; entry point for gcc
 main:
 push rbp ; sets up stack space
 mov rbp,rsp
 mov rdi, fmt
-call rax; call printf
+call printf; call the c func printf
 mov rsp, rbp
 pop rbp ; used to restore stack, further exit
 mov rax, 0
