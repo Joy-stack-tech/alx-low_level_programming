133f4f8e74cbf6573092d6531472b36602caaae3
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-30 13:18:43 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - func that finds the occurence of a substring
+ * @needle: subtring whose occurence is to be found
+ * @haystack: the main string
+ * Return: pointer to the beginning of the located string
+ * and null if @needle can not be found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *hhaystack;
+	char *nneedle;
+
+	if (*haystack != '\0')
+	{
+		hhaystack = haystack;
+		nneedle = needle;
+
+		while (*haystack != '\0' && *nneedle != '\0' && *haystack == *nneedle)
+		{
+			haystack++;
+			nneedle++;
+		}
+		if (!*nneedle)
+			return (haystack = hhaystack + 1);
+	}
+	return (0);
+}
