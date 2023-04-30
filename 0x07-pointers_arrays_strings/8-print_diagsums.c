e94f55ac2077066f625bdac152f08626be15b4d5
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-30 15:53:09 -0700

    8-print_diagsums.cc

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagsums - func that prints the sum of
+ * a square matrix
+ * @size:  the matrix column width
+ * @a: pointer to start of matrix
+ * Return: empty
+*/
+void print_diagsums(int *a, int size)
+{
+	int j, num1, num2 = 0;
+
+	num1 = 0;
+	for (j = 0; j < (size * size); j++)
+	{
+		while (j % (size + 1) == 0)
+			num1 += *(a + j);
+		if (j % (size - 1) == 0 && j != 0 && j < size * size - 1)
+			num2 += *(a + j);
+	}
+	printf("%d, %d\n", num1, num2);
+}
+
