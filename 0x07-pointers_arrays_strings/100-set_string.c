391266da1c9c5bfd59905fb41e10dfdae28f71bb
Joy-stack-tech <girlspeakingmillions@gmail.com>
2023-04-30 16:16:25 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - func that sets a pointer@s value to char
+ * @to: target address pointers value will be returned
+ * @s: the address of source
+ * Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
